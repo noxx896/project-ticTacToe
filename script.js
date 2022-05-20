7276a6ad0d2718aec5c87e1bb15b9fa9319294da
n0xx <smc.8645@gmail.com>
2022-05-20 13:56:36 -0500

    Function to display initial screen

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ const gameBoard = (() => {
 const Player = (name) => {
 
 };
+
+// Start game
+const initializeGame = (() => {
+  const content = document.querySelector('#content');
+
+  const initOptions = document.getElementById('init-options');
+  initOptions.textContent = 'Initialize content';
+
+  const initNewBtn = document.createElement('button');
+  initNewBtn.id = 'init-new-btn';
+  initNewBtn.textContent = 'New';
+  initOptions.appendChild(initNewBtn);
+
+  content.style.display = 'none';
+  initOptions.style.display = 'block';
+  
+  initNewBtn.addEventListener('click', function() {
+    initOptions.style.display = 'none';
+    content.style.display = 'flex';
+  });
+
+})();
