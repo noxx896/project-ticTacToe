f2cc5ac05565c1d64207e9d2a3f5573f02c92451
n0xx <smc.8645@gmail.com>
2022-05-26 22:13:59 -0500

    Added logic to game table buttons

--- script.js
+++ script.js
@@ -1,14 +1,172 @@
 /* script */
 
-// Module to create a board to play
-const gameBoard = (() => {
+// Module to play one game
+const playGame = (playerOne, playerTwo) => {
+  //Player's moves
+  let move = [null, null, null,
+               null, null, null,
+               null, null, null];
+  // Player turn
+  let playerTurn = false;
 
+  let moveCounter = 0;
 
-})();
+  const gameInfo = document.getElementById('game-info');
+  gameInfo.textContent = `Player 1: ${playerOne.name}\n
+                          Player 2: ${playerTwo.name}`
+
+  // Board buttons
+  const sqr_0 = document.getElementById('sqr_0');
+  const sqr_1 = document.getElementById('sqr_1');
+  const sqr_2 = document.getElementById('sqr_2');
+  const sqr_3 = document.getElementById('sqr_3');
+  const sqr_4 = document.getElementById('sqr_4');
+  const sqr_5 = document.getElementById('sqr_5');
+  const sqr_6 = document.getElementById('sqr_6');
+  const sqr_7 = document.getElementById('sqr_7');
+  const sqr_8 = document.getElementById('sqr_8');
+
+  // Button events
+  sqr_0.addEventListener('click', function() {
+    sqr_0.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[0] = 1;
+      sqr_0.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[0] = 0;
+      sqr_0.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_1.addEventListener('click', function() {
+    sqr_1.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[1] = 1;
+      sqr_1.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[1] = 0;
+      sqr_1.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_2.addEventListener('click', function() {
+    sqr_2.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[2] = 1;
+      sqr_2.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[2] = 0;
+      sqr_2.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_3.addEventListener('click', function() {
+    sqr_3.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[3] = 1;
+      sqr_3.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[3] = 0;
+      sqr_3.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_4.addEventListener('click', function() {
+    sqr_4.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[4] = 1;
+      sqr_4.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[4] = 0;
+      sqr_4.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_5.addEventListener('click', function() {
+    sqr_5.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[5] = 1;
+      sqr_5.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[5] = 0;
+      sqr_5.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_6.addEventListener('click', function() {
+    sqr_6.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[6] = 1;
+      sqr_6.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[6] = 0;
+      sqr_6.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_7.addEventListener('click', function() {
+    sqr_7.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[7] = 1;
+      sqr_7.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[7] = 0;
+      sqr_7.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  sqr_8.addEventListener('click', function() {
+    sqr_8.disabled = true;
+    if(playerTurn) {
+      playerTurn = false;
+      move[8] = 1;
+      sqr_8.textContent = 'CIRCULO'
+      moveCounter++;
+    }else {
+      playerTurn = true;
+      move[8] = 0;
+      sqr_8.textContent = 'EQUIS';
+      moveCounter++;
+    }
+  });
+  //return 0;
+
+};
 
 // Factory function to create new player
 const Player = (name) => {
+  let score = null;
+  //const getName = () => name;
+  const getScore = () => score;
+  const setScore = stat => {
+    score = stat;
+  }
 
+  return {name, getScore};
 };
 
 // Start game
@@ -41,6 +199,8 @@ const initializeGame = (() => {
 
   // Enter data
   const enterData = document.getElementById('enter-data');
+  const nameOne = document.getElementById('name-one');
+  const nameTwo = document.getElementById('name-two');
   const startGame = document.getElementById('start-game-btn');
 
   // Show/Hide content
@@ -49,6 +209,7 @@ const initializeGame = (() => {
   enterData.style.display = 'none';
   initOptions.style.display = 'block';
 
+  // Button events
   initNewBtn.addEventListener('click', function() {
     initOptions.style.display = 'none';
     selectGameMode.style.display = 'block';
@@ -60,8 +221,13 @@ const initializeGame = (() => {
   });
 
   startGame.addEventListener('click', function() {
+    let playerOne = Player(nameOne.value);
+    let playerTwo = Player(nameTwo.value);
     enterData.style.display = 'none';
     content.style.display = 'flex';
-  })
+    playGame(playerOne, playerTwo);
+  });
+
+  return 0;
 
 })();
